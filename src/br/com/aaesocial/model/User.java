710ebb70b1d400a8e62b95e797e5151915267ecf
Fernando M Souza Filho <fernando.marques@ice.ufjf.br>
2017-10-03 22:10:03 -0300

    Configurações iniciais

---
+++ src/br/com/aaesocial/model/User.java
@@ -0,0 +1,18 @@
+package br.com.aaesocial.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String photoUrl;
+
+    private LocalDate birthDate;
+
+    
+
+}

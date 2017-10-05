8cc77e03d61afdc7bf30f6e99d3d352300bbd501
arthurlorenzi <lorenzi.arthur@gmail.com>
2017-10-05 16:09:00 -0300

    Script para carga da base

---
+++ db/data.sql
@@ -0,0 +1,9 @@
+USE AAESocial;
+
+INSERT INTO user(email, password, firstName, lastName, birthDate, photoUrl, notify) VALUES('arthur@', '1234', 'Arthur', 'Lorenzi', '19950427', 'https://upload.wikimedia.org/wikipedia/en/a/a5/Zushi_Games_Logo.png', NULL);
+
+INSERT INTO user(email, password, firstName, lastName, birthDate, photoUrl, notify) VALUES('fernando@', '1234', 'Fernando', 'Marques', '19940206', 'https://i.pinimg.com/736x/6b/bb/b8/6bbbb8473690a996984b33240b33157a.jpg', NULL);
+
+INSERT INTO user(email, password, firstName, lastName, birthDate, photoUrl, notify) VALUES('fulano@', '1234', 'Fulano', 'Beltrano', '19940308', 'http://static.sbt.com.br/media/playlist/20100209115700/20100209115717/20161201182146.jpg', NULL);
+
+INSERT INTO user(email, password, firstName, lastName, birthDate, corporative, photoUrl, notify) VALUES('ufjf@', '1234', 'UFJF', '', '19601223', TRUE, 'http://www.ufjf.br/fisiologia/files/2012/05/UFJF-600-dpi-1024x740.jpg', NULL);
